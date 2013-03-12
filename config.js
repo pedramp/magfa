3b6fdce7606da69af2ac72add200a51534ebd6a7
Pedram Pourhossein <pedramp@gmail.com>
2013-03-12 20:40:11 +0330

    implementing get method

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
 	server : 
 	{
 		hostname: 'messaging.magfa.com',
-		port: 80,	// 80(HTTP) & 443(HTTPS) are acceptable.
+		port: 443,	// 80(HTTP) & 443(HTTPS) are acceptable.
 		path: '/magfaHttpService?', // Base Path of API
 		method: 'POST' // POST & GET, are acceptable.
 	},
@@ -13,9 +13,5 @@ module.exports = {
 		username : null, 
 		password : null,
 		from     : null
-	},
-	methods:
-	{
-		enqueue  : 'enqueue' // to send SMS
 	}
 }
